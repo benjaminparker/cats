3b946a18e8e5cc3976ffb98e37c903870d59e008
benjaminparker <benjamin@needstyping.com>
2019-06-28 08:45:46 +0100

    Initial commit

---
+++ build.sbt
@@ -0,0 +1,9 @@
+name := "cats"
+
+version := "0.1"
+
+scalaVersion := "2.12.0"
+
+scalacOptions += "-Ypartial-unification"
+
+libraryDependencies += "org.typelevel" %% "cats-core" % "2.0.0-M1"
---
+++ src/main/scala/ASync.scala
@@ -0,0 +1,45 @@
+import cats.data.EitherT
+import cats.instances.future._
+
+import scala.concurrent.{ExecutionContext, Future}
+
+object Async {
+
+  case class ErrorMessage(value: String) extends AnyVal
+
+  case class Thing(value: String) extends AnyVal
+
+  def findUpdateAudit(implicit ec: ExecutionContext): Future[Either[ErrorMessage, Thing]] = {
+    findAThing.flatMap {
+      case Some(thing) => upsertAThing(thing).collect {
+        case Right(updatedThing) => Right(auditAThing(updatedThing))
+      }
+      case None => Future.successful(Left(ErrorMessage("Thing not found")))
+    }
+  }
+
+  def findUpdateAuditWithCats(implicit ec: ExecutionContext): Future[Either[ErrorMessage, Thing]] = {
+    val f = for {
+      thing <- EitherT.fromOptionF(findAThing, ErrorMessage("Thing not found"))
+      updatedThing <- EitherT(upsertAThing(thing))
+      auditedThing <- EitherT.pure(auditAThing(updatedThing))
+    } yield auditedThing
+
+    f.value
+  }
+
+  def findAThing: Future[Option[Thing]] = {
+    //find a thing and if found...
+    Future.successful(Some(Thing("ABC123")))
+  }
+
+  def upsertAThing(thing: Thing): Future[Either[ErrorMessage, Thing]] = {
+    //store the thing and then if successful..
+    Future.successful(Right(thing))
+  }
+
+  def auditAThing(thing: Thing): Thing = {
+    //do some auditing
+    thing
+  }
+}
